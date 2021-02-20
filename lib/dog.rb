f93bd66d1555e5535239b7614ca6792c2c35d672
Omar Branez <omar.branez@gmail.com>
2021-02-20 16:31:50 -0500

    commit tree b82971ce45bbb4deb350f400fe687baeaa81ba97

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,31 @@
 # Add your code here
+class Dog
+    attr_accessor :all
+    attr_reader :name
+    
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        #@@all = []
+        @@all.clear
+    end
+
+    def self.print_all
+        @@all.each do |dogs| dogs.name
+            puts dogs.name
+        end
+    end
+
+    def save
+        @@all << self
+    end
+end
